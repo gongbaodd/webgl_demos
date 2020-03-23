57ee9d6f6a30c630016fb48edbc7b11fa9eb9480
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-03-23 12:05:38 +0800

    🐛 Fix: Fix css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import "../style/fix.css";
 import "antd/dist/antd.css";
+import "../style/fix.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
