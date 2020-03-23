9d54baae27c3920bc2ad703a933808a23dbaf15b
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-03-23 16:08:19 +0800

    ✨ Feat: Add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,63 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <Manifest />
+          <meta name="generator" content="Next" />
+          <title>宫不上的WebGL实验室</title>
+          <meta name="description" content="宫不上的WebGL实验室" />
+          <meta property="og:title" content="宫不上的WebGL实验室" />
+          <meta property="og:description" content="宫不上的WebGL实验室" />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:creator" content="gongbaodd" />
+          <meta name="twitter:title" content="宫不上的WebGL实验室" />
+          <meta name="twitter:description" content="宫不上的WebGL实验室" />
+          <meta name="referrer" content="no-referrer" />
+          <link
+            rel="preconnect dns-prefetch"
+            href="https://www.google-analytics.com"
+          />
+          <link rel="icon" href="/icons/icon-48x48.png" />
+          <meta name="theme-color" content="#000000" />
+          <link
+            rel="apple-touch-icon"
+            sizes="48x48"
+            href="/icons/icon-48x48.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/icons/icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="96x96"
+            href="/icons/icon-96x96.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/icons/icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="/icons/icon-192x192.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="256x256"
+            href="/icons/icon-256x256.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="384x384"
+            href="/icons/icon-384x384.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="512x512"
+            href="/icons/icon-512x512.png"
+          />
         </Head>
         <body style={{ background: "transparent" }}>
           <Main />
