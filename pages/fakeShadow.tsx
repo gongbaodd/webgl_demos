503fe21b3be719cd90c7f23efd433849f37ea591
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-04-23 16:36:14 +0800

    🐛 Fix: Add menu

--- pages/fakeShadow.tsx
+++ pages/fakeShadow.tsx
@@ -1,5 +1,13 @@
 import dynamic from "next/dynamic";
+import Layout from "../components/Layout";
+import { getStaticProps } from "../server/getMenu";
 
 const FakeShadow = dynamic(() => import("../playgrounds/fakeShadow"));
 
-export default () => <FakeShadow />;
+export default ({ menuItems }) => (
+  <Layout menuItems={menuItems}>
+    <FakeShadow />
+  </Layout>
+);
+
+export { getStaticProps };
