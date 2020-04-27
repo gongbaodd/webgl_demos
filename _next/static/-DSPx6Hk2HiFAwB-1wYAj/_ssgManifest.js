4476fbfc9e2b6ffd219ee3727abbed857946258d
Travis CI User <travis@example.org>
2020-04-27 10:06:32 +0000

    Updates

---
+++ _next/static/-DSPx6Hk2HiFAwB-1wYAj/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fbox","\u002Fcameras","\u002Fcannon","\u002FfakeShadow","\u002F","\u002FrenderTarget","\u002Fsolar"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
