5c00c040ddc02f206a62d0f15a42af07fd0a8037
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-04-27 12:40:20 +0800

    ✨ Feat: Render target

--- components/RenderTarget.tsx
+++ components/RenderTarget.tsx
@@ -5,61 +5,73 @@ import {
   MeshPhongMaterial,
   Scene,
   PerspectiveCamera,
+  DirectionalLight,
+  Mesh,
 } from "three";
-import { useFrame, render } from "react-three-fiber";
-import React, { useMemo, useRef } from "react";
+import { useFrame, render, useThree, createPortal } from "react-three-fiber";
+import React, { useMemo, useRef, useCallback } from "react";
 
-const useCube = () => useMemo(() => new BoxBufferGeometry(1, 1, 1), []);
+const createScene = () => {
+  const width = 512;
+  const height = 512;
 
-const Targets = () => {
-  const cube = useCube();
+  const renderer = new WebGLRenderTarget(width, height);
 
-  return (
-    <scene name="targetScene" background={new Color("red")}>
-      <directionalLight
-        color={new Color(0xffffff)}
-        intensity={1}
-        position={[-1, 2, 4]}
-      />
-      <mesh
-        geometry={cube}
-        material={new MeshPhongMaterial({ color: 0x44aa88 })}
-        position-x={0}
-      />
-      <mesh
-        geometry={cube}
-        material={new MeshPhongMaterial({ color: 0x8844aa })}
-        position-x={-2}
-      />
-      <mesh
-        geometry={cube}
-        material={new MeshPhongMaterial({ color: 0xaa8844 })}
-        position-x={2}
-      />
-    </scene>
-  );
+  const scene = new Scene();
+  scene.background = new Color("red");
+
+  const camera = new PerspectiveCamera(75, 1, 0.1, 5);
+  camera.position.z = 2;
+  scene.add(camera);
+
+  {
+    {
+      const light = new DirectionalLight(0xffffff, 1);
+      light.position.set(-1, 2, 4);
+      scene.add(light);
+    }
+    {
+      const geo = new BoxBufferGeometry(1, 1, 1);
+      const mat = new MeshPhongMaterial({ color: 0x44aa88 });
+      const cube = new Mesh(geo, mat);
+      scene.add(cube);
+    }
+    {
+      const geo = new BoxBufferGeometry(1, 1, 1);
+      const mat = new MeshPhongMaterial({ color: 0x8844aa });
+      const cube = new Mesh(geo, mat);
+      cube.position.x = -2;
+      scene.add(cube);
+    }
+    {
+      const geo = new BoxBufferGeometry(1, 1, 1);
+      const mat = new MeshPhongMaterial({ color: 0xaa8844 });
+      const cube = new Mesh(geo, mat);
+      cube.position.x = 2;
+      scene.add(cube);
+    }
+  }
+
+  return { renderer, scene, camera };
 };
 
-const target = new WebGLRenderTarget(512, 512);
-const camera = new PerspectiveCamera();
-const scene = new Scene();
-scene.add(camera);
-render(<Targets />, scene);
-
 const Renderer = () => {
-  const cube = useCube();
+  const { renderer, scene, camera } = useMemo(() => {
+    return createScene();
+  }, []);
 
   useFrame(({ gl }) => {
-    // gl.setRenderTarget(target);
+    gl.setRenderTarget(renderer);
+    gl.render(scene, camera);
+
+    gl.setRenderTarget(null);
   });
 
   return (
-    <group>
-      <mesh
-        geometry={cube}
-        material={new MeshPhongMaterial({ map: target.texture })}
-      />
-    </group>
+    <mesh>
+      <boxGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshPhongMaterial attach="material" map={renderer.texture} />
+    </mesh>
   );
 };
 
